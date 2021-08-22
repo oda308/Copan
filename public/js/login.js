28e37172d0908a7f0bdb780b647ba7bd23a54c64
odaki <takuo6942@gmail.com>
2021-08-22 13:03:55 +0900

    ログインページの画像差し替え処理を変更

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,10 @@
 $(function(){
 
-$('.carousel').carousel('cycle', {
-	interval: 4000
-  })
-  
+	$('#login-background img:nth-child(n+2)').css({opacity:'0'});
+		setInterval(function() {
+			$('#login-background img:first-child').fadeTo('300', 0);
+			$('#login-background img:nth-child(2)').fadeTo('300', 1);
+			$('#login-background img:first-child').appendTo('#login-background');
+		}, 4000);
+	
 });
